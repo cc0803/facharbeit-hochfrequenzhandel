e5d2fcfc6e435157518068ee7ef3bb3b8df94930
cc0803 <simongeldbach@gmail.com>
2024-04-14 17:20:18 +0200

    feat: add background video and background image

--- js/header.js
+++ js/header.js
@@ -3,9 +3,6 @@ const mobileNav = document.querySelector("header .mobile-nav");
 const menuButton = document.querySelector("header .dropdown-menu");
 const html = document.querySelector("html");
 
-console.log(mobileNav);
-console.log(returnHeaderButton);
-
 returnHeaderButton.addEventListener("click", () => {
 	mobileNav.classList.add("hidden");
 });
@@ -13,3 +10,15 @@ returnHeaderButton.addEventListener("click", () => {
 menuButton.addEventListener("click", () => {
 	mobileNav.classList.remove("hidden");
 });
+
+// check screensize of device
+
+const width = window.innerWidth;
+const titleFacharbeit = document.querySelector(".title-facharbeit");
+
+if (width >= 800) {
+	const video =
+		'<h1>Inwieweit stellen der Hochfrequenzhandel und Hedgefonds ein Risiko für Privatanleger dar?</h1><video src="./assets/startingPage.mp4" autoplay muted loop id="background-video" poster="./assets/startingPagePicture.png"></video>';
+	titleFacharbeit.innerHTML = video;
+	titleFacharbeit.classList.remove("title-background");
+}
