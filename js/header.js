b0caf5c2632a5b6ce43cb3b0b095e4407ed75191
cc0803 <simongeldbach@gmail.com>
2024-04-20 12:25:20 +0200

    feat

--- js/header.js
+++ js/header.js
@@ -17,8 +17,8 @@ const width = window.innerWidth;
 const titleFacharbeit = document.querySelector(".title-facharbeit");
 
 // if (width >= 800) {
-const video =
-	'<h1>Inwieweit stellen der Hochfrequenzhandel und Hedgefonds ein Risiko für Privatanleger dar?</h1><video src="./assets/startingPage.mp4" autoplay muted loop id="background-video" poster="./assets/startingPagePicture.png"></video><div class="facharbeit-werbung"><h4>Link zur Facharbeit</h4><a href="">Facharbeit</a></div>';
-titleFacharbeit.innerHTML = video;
-titleFacharbeit.classList.remove("title-background");
+	const video =
+		'';
+	titleFacharbeit.innerHTML = video;
+	titleFacharbeit.classList.remove("title-background");
 // }
