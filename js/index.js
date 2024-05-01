a7d1edba87fe5f9776ecdf77703b5f36cf839d47
cc0803 <simongeldbach@gmail.com>
2024-05-01 23:28:42 +0200

    feat: add intersection observer to fazit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show");
+		} else {
+			entry.target.classList.remove("show");
+		}
+	});
+});
 
+const portfolioCompanies = document.querySelectorAll(".portfolio-company");
+portfolioCompanies.forEach((company) => observer.observe(company));
